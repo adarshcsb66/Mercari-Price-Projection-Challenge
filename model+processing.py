a6c7e73330795488730ecd14d06100f5ed04e2bc
Adarsh Kumar <46217049+adarshcsb66@users.noreply.github.com>
2020-09-07 20:59:35 +0530

    Added CNN part

--- model+processing.py
+++ model+processing.py
@@ -14,3 +14,17 @@ def rmsle(y, y0):
 #create trial document
 model = gensim.models.Word2Vec (documents, size=50, window=10, min_count=2)
 model.train(documents,total_examples=len(documents),epochs=10)
+models=[0]*1
+for j in range(len(models)):
+  models[j]=Sequential()
+  models[j].add(Conv2D(32,(3,3),strides=(1,1),name="first",activation='relu',input_shape=(50,70,1)))
+  models[j].add(BatchNormalization())
+  models[j].add(AveragePooling2D((2,2),strides=(2,2)))
+  models[j].add(Conv2D(64,(5,5),strides=(1,1),activation='relu'))
+  models[j].add(BatchNormalization())
+  models[j].add(AveragePooling2D((2,2),strides=(2,2)))
+  models[j].add(Flatten())
+  models[j].add(Dense(units=512,activation='relu'))
+  models[j].add(Dense(units=256,activation='relu'))
+  models[j].add(Dense(units=10,activation='relu'))
+  models[j].compile(optimizer="adam",loss='categorical_crossentropy',metrics=['accuracy'])
